6cecbf80786a48fdb3e4c0a881d33b524e0bffbc
Gabriel Furquim da Silva <88550409+Gabriel7fs@users.noreply.github.com>
2024-09-01 19:32:44 -0300

    Initial commit

---
+++ src/test/java/com/example/backend_health_monitor/BackendHealthMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_health_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHealthMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
